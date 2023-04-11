98c0af30d0cb6dd7ad799de991d19aaf04aa9841
jairoalperez <jairoalberto98@hotmail.com>
2023-04-11 18:01:09 -0400

    Listo

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -1,23 +1,29 @@
 import React from 'react';
 
-function Articles() {
+function Articles({articles}) {
 
     return (
         <div className="card w-50 mx-auto">
             <table>
                 <thead>
-                <tr>
-                    <th>Title</th>
-                    <th>Upvotes</th>
-                    <th>Date</th>
-                </tr>
+                    <tr>
+                        <th>Title</th>
+                        <th>Upvotes</th>
+                        <th>Date</th>
+                    </tr>
                 </thead>
                 <tbody>
-                <tr data-testid="article" key="article-index">
-                    <td data-testid="article-title">Article 1 title</td>
-                    <td data-testid="article-upvotes">Article 1 title</td>
-                    <td data-testid="article-date">Article 1 title</td>
-                </tr>
+
+                    {articles.map((article, index) => {
+                        return (
+                            <tr data-testid="article" key={index}>
+                                <td data-testid="article-title">{article.title}</td>
+                                <td data-testid="article-upvotes">{article.upvotes}</td>
+                                <td data-testid="article-date">{article.date}</td>
+                            </tr>
+                        )
+                    })}
+
                 </tbody>
             </table>
         </div>
